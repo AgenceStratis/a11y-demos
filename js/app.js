9047010414d0df428916ff1d8fd75f714241ccac
Steven Mouret <steven.mouret.stratis@gmail.com>
2021-09-27 18:07:13 +0200

    Scripts > select with button

--- js/app.js
+++ js/app.js
@@ -10,6 +10,20 @@ const onChangeOnSelect = () => {
     }
 };
 
+const noOnChangeOnSelect = () => {
+    const button = document.getElementById('submit');
+    const select2 = document.getElementById('select2');
+
+    if (button && select2) {
+        button.addEventListener('click', function () {
+            if (select2.value !== '0') {
+                window.location.href = select2.value;
+            }
+        }, false);
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function() {
     onChangeOnSelect();
+    noOnChangeOnSelect();
 });

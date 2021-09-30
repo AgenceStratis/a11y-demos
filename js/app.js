b89fcbc145ee185d6d04564b218f30a7405305ab
Steven Mouret <steven.mouret.stratis@gmail.com>
2021-09-30 15:01:43 +0200

    Scripts > focus back

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const insertHtml = (element, htmlString, position) => {
+    element.insertAdjacentHTML(position, htmlString)
+};
+
 const onChangeOnSelect = () => {
     const select = document.getElementById('select');
 
@@ -34,9 +38,48 @@ const ccvaeu = () => {
     }, false);
 };
 
+const validateEmail = (mail) => {
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const errorMessage = `<p id="feo-email-error" class="error-message">Le courriel renseigné n'est pas correcte. Exemple de courriel attendu jean.dupond@gmail.com</p>`;
+    const errorMessageNode = document.getElementById('feo-email-error');
+    const feoForm = document.getElementById('feo-form');
+    const validationMessage = `<p id="feo-form-validation" tabindex="-1">Vous êtes bien inscrit à notre lettre d'information, un courriel de confirmation vient de vous être envoyé.</p>`;
+    const feoWrapper = document.getElementById('feo-wrapper');
+    const feoEmail = document.getElementById('feo-email');
+
+    if (mail.value.match(mailformat)) {
+        console.log("inscription OK");
+        if (errorMessageNode) {
+            errorMessageNode.remove();
+        }
+        feoForm.style.display = "none";
+        insertHtml(feoWrapper, validationMessage, "afterbegin");
+        const validationMessageNode = document.getElementById('feo-form-validation');
+        validationMessageNode.focus();
+    } else {
+        if (!errorMessageNode) {
+            mail.setAttribute("aria-describedby", "feo-email-error");
+            insertHtml(mail, errorMessage, "afterend");
+            feoEmail.setAttribute("aria-invalid", "true");
+        }
+        mail.focus();
+    }
+};
+
+const validateFeoForm = () => {
+    const button = document.getElementById('feo-button');
+    const inputEmail = document.getElementById('feo-email');
+
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        validateEmail(inputEmail);
+    }, false);
+};
+
 document.addEventListener("DOMContentLoaded", function() {
     onChangeOnSelect();
     noOnChangeOnSelect();
     ccvaeu();
+    validateFeoForm();
     MicroModal.init();
 });
